a01deb1a070fdab81ee6b1e9b544f17386a1be6e
Fadhil Rifqi Rabbani Pane <163860803+Belligool@users.noreply.github.com>
2025-05-05 14:20:35 +0700

    edit Item

--- src/entity/Item/Item.java
+++ src/entity/Item/Item.java
@@ -1,5 +1,23 @@
 package entity.Item;
 
 public abstract class Item {
+    private String itemName;
+    private String itemType;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
     
 }
