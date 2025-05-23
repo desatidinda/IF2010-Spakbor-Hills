6ef1234c6f6f4fbf0ae8801777c401bc5f137b65
Fadhil Rifqi Rabbani Pane <163860803+Belligool@users.noreply.github.com>
2025-05-23 16:02:42 +0700

    Add Coal + Firewood.

---
+++ src/entity/Item/Coal.java
@@ -0,0 +1,7 @@
+package entity.Item;
+
+public class Coal extends Misc{
+    public Coal(String itemName, Double buyPrice, double sellPrice){
+        super(itemName, sellPrice, sellPrice);
+    }
+}
---
+++ src/entity/Item/Firewood.java
@@ -0,0 +1,7 @@
+package entity.Item;
+
+public class Firewood extends Misc {
+    public Firewood(String itemName, Double buyPrice, double sellPrice){
+        super(itemName, buyPrice, sellPrice);
+    }
+}
