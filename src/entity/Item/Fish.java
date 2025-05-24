0b9d27f11392b54a4edfdc67205eeb6c29ed4efc
Fadhil Rifqi Rabbani Pane <163860803+Belligool@users.noreply.github.com>
2025-05-24 16:05:35 +0700

    Add fungsi biar ngecheck time range ikan bisa ditangkep.

--- src/entity/Item/Fish.java
+++ src/entity/Item/Fish.java
@@ -1,5 +1,7 @@
 package entity.Item;
 
+import main.GameClock;
+
 
 public class Fish extends Item{
     public enum Rarity{
@@ -10,13 +12,15 @@ public class Fish extends Item{
     private String weather;
     private String location;
     private String rarity;
+    private String time;
 
-    public Fish(String itemName, String season, String weather, String location, String rarity){
+    public Fish(String itemName, String season, String weather, String location, String rarity, String time){
         super(itemName, "Fish");
         this.season = season;
         this.weather = weather;
         this.location = location;
         this.rarity = rarity;
+        this.time = time;
     }
 
     public String getSeason() {
@@ -33,5 +37,25 @@ public class Fish extends Item{
 
     public String getRarity() {
         return rarity;
+    }
+
+    public boolean isCatchable(){
+        if(time.equals("Any")){
+            return true;
+        }
+
+        String[] timeRange = time.split("-");
+        int startMinute = parseTimeKeMenit(timeRange[0]);
+        int endMinute = parseTimeKeMenit(timeRange[1]);
+        int currentMinute = GameClock.getHour() * 60 + GameClock.getMinute();
+
+        return currentMinute >= startMinute && currentMinute <= endMinute;
+    }
+
+    private int parseTimeKeMenit(String timeStr){
+        String[] jamDanMenit = timeStr.split(":");
+        int hour = Integer.parseInt(jamDanMenit[0]);
+        int minute = Integer.parseInt(jamDanMenit[1]);
+        return hour * 60 + minute;
     }
 }
