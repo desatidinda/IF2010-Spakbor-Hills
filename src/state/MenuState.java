f41d0f6da61bb945e2adc619eac92604d4b990bb
fawwazay11 <fawwazaydin2016@gmail.com>
2025-05-26 21:08:39 +0700

    Update MenuState: change font and implements StateHandler

--- src/state/MenuState.java
+++ src/state/MenuState.java
@@ -3,16 +3,20 @@ package state;
 
 import entity.Player.Player;
 import main.GamePanel;
+import main.GameStates;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
-public class MenuState {
-    private GamePanel gp;
-    private Player player;
+public class MenuState implements StateHandler {
+    private final GamePanel gp;
+    private final Player player;
+    private final Font vt323;
 
     public MenuState(GamePanel gp, Player player) {
         this.gp = gp;
         this.player = player;
+        this.vt323 = new Font("VT323", Font.PLAIN, 24);
     }
 
     public void draw(Graphics2D g2) {
@@ -37,15 +41,32 @@ public class MenuState {
         gp.ui.drawPopupWindow(g2, boxX, boxY, boxWidth, boxHeight);
 
         // HEADER
-        g2.setFont(new Font("Arial", Font.BOLD, 24));
+        g2.setFont(vt323.deriveFont(Font.BOLD, 24F));
         gp.ui.drawCenteredText(g2, "PLAYER STATUS", boxX, boxY + 38, boxWidth);
 
-        g2.setFont(new Font("Arial", Font.PLAIN, 18));
+        g2.setFont(vt323.deriveFont(Font.PLAIN, 18F));
         int textY = boxY + 70;
 
         for (String line : lines) {
             gp.ui.drawCenteredText(g2, line, boxX, textY, boxWidth);
             textY += lineHeight;
         }
+    }
+
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            gp.gameState = GameStates.MAP; 
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
